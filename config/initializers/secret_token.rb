902b6e7a4689a8a5291fb98e91a6762c84728de8
Terry Meacham <zv1n.fire@gmail.com>
2014-08-02 11:21:39 -0500

    Initial app commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cards::Application.config.secret_key_base = 'dd2720f9755edb47a003113c4c63974801383a613ee0d5b9961819b27126e7c3d72c8dc248300097087c6a09a1017b209d0ffe0799b011695c10573e7f77301c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cards::Application.config.session_store :cookie_store, key: '_cards_session'
